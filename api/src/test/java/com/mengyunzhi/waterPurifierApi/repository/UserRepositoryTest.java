57a72316b1386bc06a17c200a1bec9c4013db56b
mskies <3792535@qq.com>
2017-06-06 07:48:24 +0800

    去除冗余的注解

---
+++ api/src/test/java/com/mengyunzhi/waterPurifierApi/repository/UserRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.mengyunzhi.waterPurifierApi.repository;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * Created by panjie on 17/6/6.
+ * 用户仓库测试实体
+ */
+public class UserRepositoryTest extends RepositoryTest {
+    private Logger logger = Logger.getLogger(UserRepositoryTest.class.getName());
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void save() {
+        logger.info("----- 保存测试 -----");
+        User user = new User();
+        userRepository.save(user);
+
+        assertThat(user.getId()).isNotNull();
+    }
+}
