09442bbbb123bcddd174024aa2b36ca4bf2869c6
chuhang123 <1093609364@qq.com>
2017-06-06 16:43:22 +0800

    订单实体测试

--- api/src/test/java/com/mengyunzhi/waterPurifierApi/repository/BillRepositoryTest.java
+++ api/src/test/java/com/mengyunzhi/waterPurifierApi/repository/BillRepositoryTest.java
@@ -1,22 +1,28 @@
 package com.mengyunzhi.waterPurifierApi.repository;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by chuhang on 17-6-6.
  * 订单实体仓库测试
  */
 public class BillRepositoryTest extends RepositoryTest {
+    private Logger logger = Logger.getLogger(UserRepositoryTest.class.getName());
+
     @Autowired
     private BillRepository billRepository;
 
     @Test
     public void save() {
+        logger.info("----- 保存测试 -----");
         Bill bill = new Bill();
         billRepository.save(bill);
+
+        assertThat(bill.getId()).isNotNull();
     }
 
 }
