4ee456af1f6ef757514e46777a218a97cdce8450
chuhang123 <1093609364@qq.com>
2017-06-06 20:47:53 +0800

    用水量详情实体，测试save方法

---
+++ api/src/main/java/com/mengyunzhi/waterPurifierApi/repository/FilterChipRepository.java
@@ -0,0 +1,11 @@
+package com.mengyunzhi.waterPurifierApi.repository;
+
+import io.swagger.annotations.ApiModel;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * Created by chuhang on 17-6-6.
+ */
+@ApiModel("滤芯实体仓库")
+public interface FilterChipRepository extends PagingAndSortingRepository<FilterChip, Long>{
+}
---
+++ api/src/test/java/com/mengyunzhi/waterPurifierApi/repository/FilterChipTest.java
@@ -0,0 +1,26 @@
+package com.mengyunzhi.waterPurifierApi.repository;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * Created by chuhang on 17-6-6.
+ */
+public class FilterChipTest extends RepositoryTest{
+    private Logger logger = Logger.getLogger(FilterChipRepository.class.getName());
+
+    @Autowired
+    private FilterChipRepository filterChipRepository;
+
+    @Test
+    public void save() {
+        logger.info("测试save方法");
+        FilterChip filterChip = new FilterChip();
+        filterChipRepository.save(filterChip);
+        assertThat(filterChip.getId()).isNotNull();
+    }
+
+}
