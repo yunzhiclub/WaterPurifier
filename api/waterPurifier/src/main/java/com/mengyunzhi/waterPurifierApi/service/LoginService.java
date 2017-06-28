9b0df48d8687832f4676d38e56e0257890a43e57
chuhang123 <1093609364@qq.com>
2017-06-28 22:40:42 +0800

    增加spring-sesion,redis

--- api/waterPurifier/src/main/java/com/mengyunzhi/waterPurifierApi/service/LoginService.java
+++ api/waterPurifier/src/main/java/com/mengyunzhi/waterPurifierApi/service/LoginService.java
@@ -9,6 +9,8 @@ import io.swagger.util.Json;
 public interface LoginService {
     //向微信发送http请求，获取openid和session_key
     String sendHttpToWechat(String code);
-    //根据
+    //根据openId和会话密钥生成16位的3rd_session
     String generate3RdSession(String openIdAndSessionKey);
+    //sha1加密算法
+    String sha1(String data);
 }
